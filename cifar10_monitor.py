12d3443998afe40f64a2997a87152886ef7ec90e
Kevin Degila <degilakevin@gmail.com>
2019-05-07 15:10:00 +0000

    Monitor training on Cifar-10

---
+++ cifar10_monitor.py
@@ -0,0 +1,54 @@
+# set the matplotlib backend so figures can be saved in the background
+import matplotlib
+matplotlib.use("Agg")
+
+# import the necessary packages
+from callbacks import TrainingMonitor
+from sklearn.preprocessing import LabelBinarizer
+from nn.conv.minivggnet import MiniVGGNet
+from tensorflow.keras.optimizers import SGD
+from tensorflow.keras.datasets import cifar10
+import argparse
+import os
+
+# Construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-o", "--output", required=True,
+                help="Path to the output directory")
+args = vars(ap.parse_args())
+
+# show info in the process ID
+print(f"[INFO processID: {os.getpid()}")
+
+# load the training and testing data, then scale it into the range [0,1]
+(trainX, trainY), (testX, testY) = cifar10.load_data()
+trainX = trainX.astype("float") / 255.0
+testX = testX.astype("float") / 255.0
+
+# convert the labels from integers to vectors
+lb = LabelBinarizer()
+trainY = lb.fit_transform(trainY)
+testY = lb.transform(testY)
+
+# initialize the label names for the CIFAR-10 dataset
+labelNames = ["airplane", "automobile", "bird", "cat", "deer",
+              "dog", "frog", "horse", "ship", "truck"]
+
+
+# initialise the SGD optimize, but without any learning rate decay
+print("[INFO] Compiling model...")
+opt = SGD(lr=0.01, momentum=0.9, nesterov=True)
+model = MiniVGGNet.build(width=32, height=32, depth=3, classes=10)
+model.compile(loss="categorical_crossentropy",
+              optimizer=opt,
+              metrics=["accuracy"])
+
+# construct the set of callbacks
+figPath = os.path.sep.join([args["output"], "{}.png".format(os.getpid)])
+jsonPath = os.path.sep.join([args["output"], "{}.json".format(os.getpid)])
+callbacks = [TrainingMonitor(figPath, jsonPath)]
+
+# train the network
+print('[INFO] training network...')
+model.fit(trainX, trainY, validation_data=(testX, testY),
+          batch_size=64, epochs=20, callbacks=callbacks, verbose=1)
