a64011f7c3975a6a63ef138a517b92710e408270
Kevin Degila <degilakevin@gmail.com>
2019-05-16 13:00:23 +0000

    Typo

--- nn_xor.py
+++ nn_xor.py
@@ -7,7 +7,7 @@ X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
 y = np.array([[0], [1], [1], [0]])
 
 nn = NeuralNetwork([2, 2, 1], alpha=0.5)
-nn.fit(X,y, epochs=20000)
+nn.fit(X, y, epochs=20000)
 
 for x, target in zip(X, y):
     pred = nn.predict(x)[0][0]
